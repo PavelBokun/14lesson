3230dcfb6930a2f5e9bf51d1db072b443d2d12b6
PavelBokun <pavel.snabteh@gmail.com>
2023-08-03 07:31:23 +0300

    first commit14

---
+++ src/state/store.ts
@@ -0,0 +1,22 @@
+import { useDispatch } from 'react-redux';
+import { tasksReducer } from './tasks-reducer';
+import { todolistsReducer } from './todolists-reducer';
+import {AnyAction, applyMiddleware, combineReducers, createStore, legacy_createStore} from 'redux';
+import thunkMidddleware, { ThunkDispatch } from 'redux-thunk'
+// объединяя reducer-ы с помощью combineReducers,
+// мы задаём структуру нашего единственного объекта-состояния
+const rootReducer = combineReducers({
+    tasks: tasksReducer,
+    todolists: todolistsReducer
+})
+// непосредственно создаём store
+export const store = legacy_createStore(rootReducer,applyMiddleware(thunkMidddleware));
+// определить автоматически тип всего объекта состояния
+export type AppRootStateType = ReturnType<typeof rootReducer>
+
+type AppDispatchType= ThunkDispatch<AppRootStateType,unknown,AnyAction>
+export const useAppDispatch=useDispatch<AppDispatchType>
+
+// а это, чтобы можно было в консоли браузера обращаться к store в любой момент
+// @ts-ignore
+window.store = store;
---
+++ src/tests/unit.test.ts
@@ -0,0 +1,18 @@
+import { FilterValuesType, setTodolistsAC } from "../state/todolists-reducer";
+
+test('test', () => {
+    const a = 1
+    const b = 2
+    const c = 3
+
+    expect(a).toBe(1);
+    expect(b).toBe(2);
+    //wrong toBe
+    expect(c).toBe(2);
+})
+
+// test('todolist ', ()=>{
+   
+
+//     const action= setTodolistsAC(startState)
+// })
